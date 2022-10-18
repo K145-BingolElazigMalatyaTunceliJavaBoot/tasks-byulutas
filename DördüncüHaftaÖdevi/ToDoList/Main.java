8ca7eaf64880b33ca189b609bcb60aafdc82bf37
Byulutas <ulutasbayram7@gmail.com>
2022-10-18 14:55:27 +0300

    Test

---
+++ DördüncüHaftaÖdevi/ToDoList/Main.java
@@ -0,0 +1,15 @@
+import entities.Pegasus;
+import entities.Person;
+import entities.Thy;
+import service.BuyService;
+import service.FoodService;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
