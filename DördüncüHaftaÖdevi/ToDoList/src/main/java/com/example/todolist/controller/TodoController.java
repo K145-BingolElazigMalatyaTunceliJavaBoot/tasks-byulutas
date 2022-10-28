5bc5edee2f584ae19069fd4bcb6a70a0a823bf7b
Byulutas <ulutasbayram7@gmail.com>
2022-10-28 15:36:37 +0300

    Dördüncü Hafta Ödevi Güncellendi.

--- DördüncüHaftaÖdevi/ToDoList/src/main/java/com/example/todolist/controller/TodoController.java
+++ DördüncüHaftaÖdevi/ToDoList/src/main/java/com/example/todolist/controller/TodoController.java
@@ -3,6 +3,7 @@ package com.example.todolist.controller;
 import com.example.todolist.dto.TodoItemDto;
 import com.example.todolist.model.TodoItem;
 import com.example.todolist.service.TodoService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -20,30 +21,43 @@ public class TodoController {
     }
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<TodoItem> findAll(){
         return todoService.findAll();
     }
+
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public TodoItem save(@Valid @NotNull @RequestBody TodoItemDto todoItemDto){
         return todoService.save(todoItemDto);
     }
+
     @PutMapping
+    @ResponseStatus(HttpStatus.OK)
     public TodoItem update(@Valid @NotNull @RequestBody TodoItemDto todoItemDto){
         return todoService.save(todoItemDto);
     }
+
     @DeleteMapping(value = "/(id)")
-    public  void  delete(@PathVariable Long id){
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public  void  delete(@PathVariable Long id) {
         todoService.delete(id);
     }
+
     @GetMapping("/sort-by-status")
+    @ResponseStatus(HttpStatus.OK)
     public List<TodoItemDto> getAllToDoByStatus(@RequestParam boolean status){
         return todoService.findAllToDoByStatus(status);
     }
+
     @GetMapping("/sort-by-day")
+    @ResponseStatus(HttpStatus.OK)
     public List<TodoItemDto> getAllToDoByDay(@RequestParam String day){
         return todoService.findAllToDoByDay(day);
     }
+
     @GetMapping("/sort-by-weekly")
+    @ResponseStatus(HttpStatus.OK)
     public List<TodoItemDto> getAllToDoByWeekly(){
         return todoService.findAllToDoByWeekly();
     }
